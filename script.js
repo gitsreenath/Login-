4dc7d71dd3b651801000c4555d3070fb554a0310
sreenath <sreenath2115@gmail.com>
2022-10-19 15:12:25 +0530

    userlogin

---
+++ script.js
@@ -0,0 +1,40 @@
+// function a(){
+//     for(var i=0;i<5;i++)
+//     {
+//         // console.log(i);
+//      }
+//     // console.log(i);
+// }
+
+// console.log(i);
+// a();
+
+// const  emp={empname:"sreenath",
+// empage:32,empid:123456,
+// fullname:function() {
+//     return (this.empname + this.empage)
+    
+    
+// }}
+
+
+// document.querySelectorAll('h1')[0].innerHTML=emp.fullname()
+
+// let a=10;
+// let b='10';
+// if(a===b){
+//     console.log('both are same');
+// }
+// else{
+//     console.log("both are different types");
+// }
+
+
+// console.log(typeof(b));
+
+// function showme(params) {
+//     window.open("https://www.google.com/");
+// }
+    
+let str="Hello,How are you";
+console.log(str.split())
